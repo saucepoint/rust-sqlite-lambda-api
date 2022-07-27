de4518d0d073eb7e87470fc504a3af655e6d02e7
saucepoint <saucepoint@protonmail.com>
2022-07-27 18:34:32 -0400

    rename resources to avoid collision

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,8 @@ provider "aws" {
 # -----------------------------------------------------------------------------
 # IAM Role for the Lambda Function
 # -----------------------------------------------------------------------------
-resource "aws_iam_role" "rust-lambda-api" {
-  name = "rust-lambda-api"
+resource "aws_iam_role" "rust-sqlite-lambda-api" {
+  name = "rust-sqlite-lambda-api"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -37,7 +37,7 @@ EOF
 
 resource "aws_iam_policy_attachment" "attach-basiclambda" {
   name = "attach-basiclambda"
-  roles = ["${aws_iam_role.rust-lambda-api.name}"]
+  roles = ["${aws_iam_role.rust-sqlite-lambda-api.name}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -46,7 +46,7 @@ resource "aws_iam_policy_attachment" "attach-basiclambda" {
 # ------------------------------------------------------------
 resource "aws_lambda_function" "lambda-api" {
   function_name = "lambda-api"
-  role = aws_iam_role.rust-lambda-api.arn
+  role = aws_iam_role.rust-sqlite-lambda-api.arn
   handler = "bootstrap"
   runtime = "provided.al2"
   filename = "../lambda-api/target/lambda/lambda-api/bootstrap.zip"
